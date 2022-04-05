e8fe1379e09cf21766fdbd00a3c394f214e1c6a8
Remelie Bongcawil <100978366+Remelie@users.noreply.github.com>
2022-04-05 19:41:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  function __destruct() {
+    echo "I am an {$this->name}. <br> My color is {$this->color}."; 
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit("Apple", "red");
+?>
